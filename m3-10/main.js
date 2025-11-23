20a5c2add6cbb9eb897abb177a5e523a4c704df9
edem222 <x477xx@gmail.com>
2025-11-23 17:12:48 +0300

    Add files via upload

---
+++ m3-10/main.js
@@ -0,0 +1,18 @@
+// Задание 1
+const firstName = "Adam";
+const lastName = "Aroken";
+let isStudent = true;
+// Задание 2
+let age = 19;
+let currentYear = 2025;
+let birthYear = currentYear - age;
+// Задание 3
+console.log(`${firstName} ${lastName}, мне ${age} лет. Я ученик ${isStudent}`)
+// Задание 4
+let a = "123";
+let b = +"456";
+let c = Number("789");
+let d = Boolean(0);
+let e = Boolean(" ");
+let result = a + b + c + d + e;
+console.log(result)
